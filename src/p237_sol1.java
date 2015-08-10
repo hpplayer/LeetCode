33fbb160b792513ab886862f51629e98d03671c7
hpPlayer <hpplayer@live.cn>
2015-08-10 01:20:34 -0500

    half finish p239

--- src/p237_sol1.java
+++ src/p237_sol1.java
@@ -1,4 +1,32 @@
+/*
+Delete Node in a Linked List 
+
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+the linked list should become 1 -> 2 -> 4 after calling your function.
+*/
+
+/**
+ *  My original AC solution without help
+ *  This problem asks ask to remove the node without giving the head node
+ *  So the idea of changing pointer/values comes to my mind.
+ *  But in the first try, I let the loop continue until the tail, then set tail node to null.
+ *  This operations turns out to be incorrect since the operation tail = null is not changing the tail node but instead let 
+ *  tail variable points to null, so the previous node in list still points to the original tail node.
+ *  
+ *  Then I changed the code as below which become accepted.
+ *  
+ *  But here I use the while loop to change the value, which is actually redundant
+ *  We can actually just operate on two nodes.
+ *  
+ *  Sol2 lists such the idea 
+ *  Sol3 is the python version of sol2
+ * @author hpPlayer
+ * @date Aug 10, 2015 12:28:18 AM
+ */
 public class p237_sol1 {
+	
 	public static class ListNode {
 		int val;
 		ListNode next;
@@ -23,7 +51,8 @@ public class p237_sol1 {
 		if (node == null || node.next == null)
 			return;
 
-		// while not tail
+		//node = null is setting node ref to null, but prev node is still pointing to the original node
+		//so we have to stop before the tail node, i.e. last second node
 		while (node.next.next != null) {
 			node.val = node.next.val;
 			node = node.next;
---
+++ src/p239_sol1.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+/*
+Sliding Window Maximum 
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+For example,
+Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Therefore, return the max sliding window as [3,3,5,5,6,7].
+
+Note: 
+You may assume k is always valid, ie: 1 ¡Ü k ¡Ü input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+		
+Hint:
+
+1. How about using a data structure such as deque (double-ended queue)?
+2. The queue size need not be the same as the window¡¯s size.
+3. Remove redundant elements and the queue should store only elements that need to be considered.
+*/
+
+/**
+ * This is my original AC solution without help
+ * I used maxHeap implemented with PriorityQueue
+ * I create a maxHeap for the first k elements, fill the result array with the top max value
+ * Then move the window, remove the first element in the windows from heap and add the new element in the window to the heap
+ * Repeat above steps until our result array get filled up.
+ * 
+ * Remark:
+ * 1) I observed there will be len - k + 1 windows in the array, so we can create a result array with such length  
+ * 2) Remove(object) in maxHeap is linear time
+ * But add and remove(head) in maxHeap is O(log(n)) time
+ * So the total running time should be the combination of them and may exceed O(n)
+ * But fortunately it got accepted.
+ * 
+ * More fantastic solution will come tomorrow!
+ * @author hpPlayer
+ * @date Aug 10, 2015 1:08:23 AM
+ */
+public class p239_sol1 {
+	public static void main(String[] args){
+		int[] nums = {1,3,-1,-3,5,3,6,7};
+		int k = 3;
+		
+		System.out.println(Arrays.toString(new p239_sol1().maxSlidingWindow(nums, k)));
+	}
+    public int[] maxSlidingWindow(int[] nums, int k) {
+    	if (nums.length == 0) return new int[0];
+        int[] result = new int[nums.length - k + 1];
+        
+        PriorityQueue<Integer> maxheap = new PriorityQueue<Integer>(10, Collections.reverseOrder());
+     
+        for(int i = 0; i < k; i++){
+            maxheap.add(nums[i]);
+        }
+        
+        result[0] = maxheap.peek();
+        for(int i = 1; i < result.length; i++){
+            maxheap.remove(nums[i-1]);
+            maxheap.add(nums[k-1+i]);
+            result[i] = maxheap.peek();
+        }
+        
+        return result;
+    }
+}
