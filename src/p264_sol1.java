3ef9cc72347096260fe20bbd195ed7848d07fc4c
hpPlayer <hpplayer@live.cn>
2015-08-20 22:34:18 -0700

    Half Finish 264

--- src/p264_sol1.java
+++ src/p264_sol1.java
@@ -1,4 +1,21 @@
 import java.util.LinkedList;
+/*
+Ugly Number II 
+
+Write a program to find the n-th ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+
+Note that 1 is typically treated as an ugly number.
+
+Hint:
+
+The naive approach is to call isUgly for every number until you reach the nth one. Most numbers are not ugly. Try to focus your effort on generating only the ugly ones.
+An ugly number must be multiplied by either 2, 3, or 5 from a smaller ugly number.
+The key is how to maintain the order of the ugly numbers. Try a similar approach of merging from three sorted lists: L1, L2, and L3.
+Assume you have Uk, the kth ugly number. Then Uk+1 must be Min(L1 * 2, L2 * 3, L3 * 5).
+*/
+
 /**
  * This is a very brilliant solution that with the help of double linked list structure
  * What we do is creating the newest ugly number by taking the largest potential number in linkedlist.
@@ -14,6 +31,7 @@ import java.util.LinkedList;
  * we found the largest potential candidates for those three primes are  1, 1, 1
  * So we can get three new ugly numbers: 1*2, 1*3, 1*5 
  * inserting the smallest one into the linkedlist
+ * 
  * @author hpPlayer
  * @date Aug 19, 2015 8:23:01 PM
  */
---
+++ src/p264_sol2.java
@@ -0,0 +1,71 @@
+/**
+ * This is the modified merge sort algorithm, which can also be treated as Dynamic programming since it uses the value from previous computation
+ * Ugly numbers can only be produced by previous ugly numbers, otherwise it will not be treated as ugly (think about p263, how we define the ugly number)
+ * So, based on the basic prime number, we can split the complete ugly number into three sequences:
+ * Total list: 1, 2, 3, 4, 5, 6, 8, ....
+ * Sequence 1: 1*2, 2*2, 3*2, 4*2, 5*2, 6*2, 8*2
+ * Sequence 2: 1*3, 2*3, 3*3, 4*3, 5*3, 6*3, 8*3
+ * Sequence 3: 1*5, 2*5, 3*5, 4*5, 5*5, 6*5, 8*5
+ * 1, 2, 3, 4, 5, 6,8 are first seven numbers in the sequence.
+ * 1 in total list in the initial case, which we already define 1 can be treated as ugly number
+ * In each sequence, we will simply create a new ugly number by following the created total list and multiplying the corresponding prime number
+ * So this is like, we are creating the total list by splitting the previous total list into three sequences(updating three subsequences),
+ * then merging them and creating the new element in total list
+ * Ex:
+ * Inital case in total list: 1
+ * we splitting 1 into three sequences, and getting new ugly number 2,3,5
+ * seq1: 1*2
+ * seq2: 1*3
+ * seq5: 1*5
+ * Then we merge 2 into total list, now the total list: 1,2
+ * We splitting 2 into three sequences:
+ * seq1 2*2
+ * seq2 1*3, 2*3
+ * seq3 1*5, 2*5
+ * Then we merge 3 into total list, now the total list: 1,2,3
+ * We splitting 3 into three sequences:
+ * seq1 2*2, 2*3
+ * seq2 2*3, 3*3
+ * seq3 1*5, 2*5, 3*3
+ * and so on....
+ * 
+ * Remark:
+ * 1) this algorithm is at least very similar to merge sort, but with a little modification
+ * 2) Each time we will pick the smallest head elements in three sequences into our total list
+ * 3) Sometimes, head elements maybe identical for two or more sequences, in this case, we will pick all of them, so all sequences will be 
+ * updated with new head elements
+ * 4) Time complexity: O(n), Space complexity: O(n)
+ * 
+ * So based on the clear idea, fast algorithm and concise code of sol2, sol2 is the best solution towards this problem!
+ * @author hpPlayer
+ * @date Aug 20, 2015 6:38:27 PM
+ */
+public class p264_sol2 {
+    public int nthUglyNumber(int n) {
+    	int[] ugly = new int[n];
+    	int num2 = 2, num3= 3, num5 = 5;//current number got from multiplying primes
+    	int index2 = 0, index3 = 0, index5 = 0;
+    	ugly[0] = 1;
+    	for(int i = 1; i < n; i ++){
+    		//pick the smallest element in three sequences and insert into the merged sequence
+    		int newUgly = Math.min(num2, Math.min(num3, num5));
+    		ugly[i] = newUgly;
+    		//We use one element in num2 sequence so we need move index2 one step further and points to the next num2 element
+    		if (newUgly == num2){
+    			num2 = 2 * ugly[++index2];
+    		}
+    		//Notice: here we use three ifs not if-else if, why?
+    		//Because we may have same elements in several sequences, and we will take all of them from sequences, so we will check each sequence one by one
+    		if (newUgly == num3){
+    			//We use one element in num3 sequence so we need move index3 one step further and points to the next num3 element
+    			num3 = 3*ugly[++index3];
+    		}
+    		if (newUgly == num5){
+    			//We use one element in num5 sequence so we need move index5 one step further and points to the next num5 element
+    			num5 = 5*ugly[++index5];
+    		}
+    	}
+    	
+    	return ugly[n-1];
+    	}
+}
