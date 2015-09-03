100b7c33bd96e9f14de0845fe3bd2e641a333e71
hpPlayer <hpplayer@live.cn>
2015-09-03 12:53:28 -0500

    Finish p054

--- src/p054_sol1.java
+++ src/p054_sol1.java
@@ -1,10 +1,48 @@
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+Spiral Matrix 
+
+
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
+*/
+
+		
 /**
  * This is my original AC solution without help!!
  * So happy, I made it!!!
  * haha, I believe my algorithm is very good.
  * But i will check other's solution tomorrow, stay tuned!
+ * 
+ * Update:
+ * 
+ * I checked all solutions post online, recursive, python' short code
+ * But they are all no better/not implementable in java, so I will still stick to my solution
+ * 
+ * The main idea is we use found bounds of the scan
+ * when scan from left->right, we will use right bound
+ * when scan from top->bottom, we will use bottom bound
+ * when scan from right->left, we will use left bound
+ * when scan from bottom -> top, we will use top bound
+ * Each scan will update the bound to shrink the next scan range
+ * So we just need a big while loop which will continue scan the board within ranges
+ * The loop will be break when two bounds meet (like right meets left or top meets bottom)
+ * 
+ * What should be noticed, each while loop will change 4 bounds, but in the middle of while loop,
+ * we may already get the while-break condition, so i put for if statements in the middle
+ * 
+ * My algorithm only scan each cell once, so the total running time should be O(mn)
  * 
  * @author hpPlayer
  * @date Sep 3, 2015 1:14:00 AM
